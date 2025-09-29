cbaf4f3a047517c9f60264364157f4d4e412d957
mka <mka@A21PC16>
2025-09-29 17:24:34 +0300

    smth

--- 01_TaskManeger/MainWindow.xaml.cs
+++ 01_TaskManeger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,17 +24,23 @@ namespace _01_TaskManeger
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            grid.ItemsSource = Process.GetProcesses();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            (grid.SelectedItem as Process).Kill();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            Process process = (grid.SelectedItem as Process);
+            string message = " ";
+            message += process.MachineName;
 
+            MessageBox.Show(message, "Process info", MessageBoxButton.OK, MessageBoxImage.Information);
         }
+
     }
+}
 }
